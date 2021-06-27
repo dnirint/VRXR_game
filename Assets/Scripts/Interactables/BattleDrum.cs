300876e69f9d9791f022e0eb1b05f1243a7ab23e
Erez Hecht <erez.hecht@gmail.com>
2021-06-27 17:52:48 +0300

    Drum rolls please
    Added different drum lanes + random weighted drum picking

--- Assets/Scripts/Interactables/BattleDrum.cs
+++ Assets/Scripts/Interactables/BattleDrum.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BattleDrum : MonoBehaviour
 {
 
+    public int priority = 0; 
+    
     void Start()
     {
         GetComponent<DrumScript>().drumCollision.PlayerTouchedDrum.AddListener(() => { BossToPlayerInteractions.Instance.DestroyClosestProjectileOnSameLane(gameObject); });
