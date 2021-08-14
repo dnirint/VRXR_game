f3c6b325c101b3ea887a15b2fdf5e6beca00b57a
Erez Hecht <erez.hecht@gmail.com>
2021-08-14 16:43:02 +0300

    Color inversion working

---
+++ Assets/Scripts/Visualizers/ColorInversion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorInversion : MonoBehaviour
+{
+    private bool inverted = false;
+    public GameObject inversionField;
+    void Start()
+    {
+        PlayerMovement.Instance.TeleportMidway.AddListener(InverseColor);    
+        inversionField.SetActive(inverted);
+    }
+
+    void InverseColor()
+    {
+        inverted = !inverted;
+        inversionField.SetActive(inverted);
+    }
+
+
+
+}
